cd41ae9ddf1ecaf75a88f880dbf061044eda2798
AK391 <81195143+AK391@users.noreply.github.com>
2021-11-26 01:56:31 -0500

    Update paths_config.py

--- config/paths_config.py
+++ config/paths_config.py
@@ -1,4 +1,4 @@
 
 model_paths = {
-	'shape_predictor': '/Users/hulk/Documents/InKe/scripts/align/shape_predictor_68_face_landmarks.dat',
+	'shape_predictor': './shape_predictor_68_face_landmarks.dat',
 }
